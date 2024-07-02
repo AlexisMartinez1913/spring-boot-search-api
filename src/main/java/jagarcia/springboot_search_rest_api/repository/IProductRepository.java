9e435654c0b2e17f7017853d1b95fa29676d16db
Alexis Martinez <jhonyalexis100@gmail.com>
2024-07-02 18:50:14 -0500

    create JPQL and Native SQL Queries to Search Products

--- src/main/java/jagarcia/springboot_search_rest_api/repository/IProductRepository.java
+++ src/main/java/jagarcia/springboot_search_rest_api/repository/IProductRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface IProductRepository extends JpaRepository<Product, Long> {
+
+    //JPQL Query (Java Persistence Query Language) 
     @Query("SELECT p FROM Product p WHERE " +
             "p.name LIKE CONCAT('%', :query, '%') OR " +
             "p.description LIKE CONCAT('%', :query, '%')")
     List<Product> searchProducts(String query);
 
+    //Native SQL Query
     @Query(value = "SELECT * FROM products p WHERE " +
             "p.name LIKE CONCAT('%', :query, '%') OR " +
             "p.description LIKE CONCAT('%', :query, '%')", nativeQuery = true)
